4aebaad6756aa6e920862537b847f7a64a01c8bb
Mario <mariovukelic@yahoo.com>
2020-09-28 23:21:03 +0200

    contract migrated to ganashe

---
+++ abi.js
@@ -0,0 +1,177 @@
+var abi = [
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "messageToPlayer",
+          "type": "string"
+        }
+      ],
+      "name": "coinFlipResult",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "messageToPlayer",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "toTransfer",
+          "type": "uint256"
+        }
+      ],
+      "name": "fundsSentToPlayer",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "adr",
+          "type": "address"
+        }
+      ],
+      "name": "playerRegistered",
+      "type": "event"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "balance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "creators",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "depositFunds",
+      "outputs": [],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "register",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "bool",
+          "name": "betOnHead",
+          "type": "bool"
+        }
+      ],
+      "name": "flipCoin",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getPlayerData",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "won",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "lost",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "plays",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "withdrawAll",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-$(document).ready(function() {
+var web3 = new Web3(Web3.givenProvider);
+console.log("web3 version = " + web3.version);
+var contractInstance;
 
+$(document).ready(function() {
+    window.ethereum.enable().then(function(accounts){
+        console.log("Account = " + accounts[0]);
+        if (accounts[0] != "0x32Dc0A2beFDADc2C76b1D034ae354A97DDa80Ba6") {
+            console.log("Error: Owner account is wrong!!!!");
+        }
+        console.log("Account isConnected = " + window.ethereum._state.isConnected);
+      contractInstance = new web3.eth.Contract(abi, "0x484db5b2627De962fCa7517087D913e1C800faA7", {from: accounts[0]});
+      console.log(contractInstance);
+      console.log(contractInstance.methods);
+    });
     $("#register").click(registerWithContract);
     $("#bet-head").click(betOnHead);
     $("#bet-tail").click(betOnTail);
-    
 });
 
 function registerWithContract(){
     var userAddr = $("#user_eth_addr").val();
     console.log("click register");
     console.log(userAddr);
-    $("#userAccountStatus").text("10000000");
+    let contractBalance = contractInstance.balance.call()
+    $("#poolMax").text(contractBalance);
     
 }
 function betOnTail(){
